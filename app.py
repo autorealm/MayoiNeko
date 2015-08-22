a36edd42d3f49bcfeb23a471469d3cb2a1a646b8
KYO <autorealm@163.com>
2015-08-22 20:35:36 +0800

    fixed bug

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ from flask import redirect
 from flask import render_template
 
 from views.todos import todos_view
-from views.blogs import blogs_view
-from views.manages import manages_view
 
 import develop.apis
 
@@ -31,38 +29,3 @@ def index():
 @app.route('/time')
 def time():
     return str(datetime.now())
-
-@app.route('/signin')
-def signin():
-    return render_template('signin.html', params=dict())
-
-@app.route('/register')
-def register():
-    return render_template('register.html', params=dict())
-
-@app.route('/blog/<blog_id>/update', methods=['POST'])
-def blog_update(blog_id):
-    i = request.input(name='', summary='', content='')
-    name = i.name.strip()
-    summary = i.summary.strip()
-    content = i.content.strip()
-    return apis.update_blog(blog_id, name, summary, content)
-
-@app.route('/blog/<blog_id>/delete', methods=['POST', 'GET'])
-def blog_delete(blog_id):
-    return apis.delete_blog(blog_id)
-
-@app.route('/blog/<blog_id>/comment', methods=['POST'])
-def comment(blog_id):
-    content = request.input(content='').content.strip()
-    return apis.comment(blog_id, content)
-
-@app.route('/comment/<comment_id>/update', methods=['POST'])
-def comment_update(comment_id):
-    content = request.input(content='').content.strip()
-    return apis.update_comment(comment_id, content)
-
-@app.route('/comment/<comment_id>/delete', methods=['DELETE'])
-def comment_delete(comment_id):
-    return apis.delete_comment(comment_id)
-
